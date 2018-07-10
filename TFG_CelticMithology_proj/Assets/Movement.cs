051c0637bf578e39c38f3cae242dd7b7d29bd7f7
nicobabot <nico.babotaltisen@gmail.com>
2018-07-10 12:27:19 +0200

    Starting walkability test

---
+++ TFG_CelticMithology_proj/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 temp_pos = transform.position;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            temp_pos.x += Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            temp_pos.x -= Time.deltaTime * speed;
+        }
+
+        transform.position = temp_pos;
+
+    }
+}
