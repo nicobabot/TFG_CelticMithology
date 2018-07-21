4f2bcb1a1b7a6fccadbc94b03cc5d736766ffe8e
nicobabot <nico.babotaltisen@gmail.com>
2018-07-21 10:13:57 +0200

    Movement without diagonals completed

--- TFG_CelticMithology_proj/Assets/Movement.cs
+++ TFG_CelticMithology_proj/Assets/Movement.cs
@@ -164,9 +164,26 @@ public class Movement : MonoBehaviour {
                 break;
             case Direction.UP:
 
+                Vector3Int tile_top_right_up = Vector3Int.zero;
+                tile_top_right_up = walkability.LocalToCell(sprite_top_right);
+                tile_top_right_up.y += 1;
+
+                Vector3Int tile_top_left_up = Vector3Int.zero;
+                tile_top_left_up = walkability.LocalToCell(sprite_top_left);
+                tile_top_left_up.y += 1;
+
+                ret = UpComprovation(sprite_top_right, sprite_top_left, tile_top_right_up, tile_top_left_up);
                 break;
             case Direction.DOWN:
+                Vector3Int tile_bottom_right_down = Vector3Int.zero;
+                tile_bottom_right_down = walkability.LocalToCell(sprite_bottom_right);
+                tile_bottom_right_down.y -= 1;
 
+                Vector3Int tile_bottom_left_down = Vector3Int.zero;
+                tile_bottom_left_down = walkability.LocalToCell(sprite_bottom_left);
+                tile_bottom_left_down.y -= 1;
+
+                ret = DownComprovation(sprite_bottom_right, sprite_bottom_left, tile_bottom_right_down, tile_bottom_left_down);
                 break;
 
         }
@@ -268,6 +285,114 @@ public class Movement : MonoBehaviour {
 
 
             if (sprite_bottom_left.x > tile_x_comprovation)
+            {
+                check2_bottom = true;
+            }
+        }
+
+        if (check1_top == true && check2_bottom == true)
+        {
+            ret = true;
+        }
+
+
+        return ret;
+    }
+
+    bool UpComprovation(Vector3 sprite_top_right, Vector3 sprite_top_left, Vector3Int tile_top_right_pos, Vector3Int tile_top_left_pos)
+    {
+        bool ret = false;
+        bool check1_top = false;
+        bool check2_bottom = false;
+
+        TileBase tile_top_right = walkability.GetTile(tile_top_right_pos);
+        TileBase tile_top_left = walkability.GetTile(tile_top_left_pos);
+
+        if (tile_top_right == walkable_tile)
+        {
+            check1_top = true;
+
+        }
+        else
+        {
+            Vector3 world_position_tile = walkability.GetCellCenterWorld(tile_top_right_pos);
+            //Need to find tile width and height units without magic number
+            float tile_y_comprovation = world_position_tile.y - (1.5f / 2);
+
+            if (sprite_top_right.y < tile_y_comprovation)
+            {
+                check1_top = true;
+            }
+
+        }
+
+        if (tile_top_left == walkable_tile)
+        {
+            check2_bottom = true;
+        }
+        else
+        {
+            Vector3 world_position_tile = walkability.GetCellCenterWorld(tile_top_left_pos);
+
+            //Need to find tile width and height units without magic number
+            float tile_y_comprovation = world_position_tile.y - (1.5f / 2);
+
+
+            if (sprite_top_left.y < tile_y_comprovation)
+            {
+                check2_bottom = true;
+            }
+        }
+
+        if (check1_top == true && check2_bottom == true)
+        {
+            ret = true;
+        }
+
+
+        return ret;
+    }
+
+    bool DownComprovation(Vector3 sprite_bottom_right, Vector3 sprite_bottom_left, Vector3Int tile_bottom_right_pos, Vector3Int tile_bottom_left_pos)
+    {
+        bool ret = false;
+        bool check1_top = false;
+        bool check2_bottom = false;
+
+        TileBase tile_bottom_right = walkability.GetTile(tile_bottom_right_pos);
+        TileBase tile_bottom_left = walkability.GetTile(tile_bottom_left_pos);
+
+        if (tile_bottom_right == walkable_tile)
+        {
+            check1_top = true;
+
+        }
+        else
+        {
+            Vector3 world_position_tile = walkability.GetCellCenterWorld(tile_bottom_right_pos);
+            //Need to find tile width and height units without magic number
+            float tile_y_comprovation = world_position_tile.y + (1.5f / 2);
+
+            if (sprite_bottom_right.y > tile_y_comprovation)
+            {
+                check1_top = true;
+            }
+
+        }
+
+        if (tile_bottom_left == walkable_tile)
+        {
+            check2_bottom = true;
+        }
+        else
+        {
+            Vector3 world_position_tile = walkability.GetCellCenterWorld(tile_bottom_left_pos);
+
+            //Need to find tile width and height units without magic number
+            float tile_y_comprovation = world_position_tile.y + (1.5f / 2);
+
+
+            if (sprite_bottom_left.y > tile_y_comprovation)
             {
                 check2_bottom = true;
             }
