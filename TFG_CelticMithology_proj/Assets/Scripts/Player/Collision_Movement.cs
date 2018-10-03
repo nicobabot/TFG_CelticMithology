3051f3c7926c21438d8ccbcfa04cc4196a15d79e
nicobabot <nico.babotaltisen@gmail.com>
2018-10-03 16:46:59 +0200

    Testing the new dash

--- TFG_CelticMithology_proj/Assets/Scripts/Player/Collision_Movement.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/Player/Collision_Movement.cs
@@ -5,22 +5,21 @@ using UnityEngine;
 public class Collision_Movement : MonoBehaviour {
 
     Rigidbody2D rb;
-    public float speed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	public void Movement_Update (float new_speed) {
 
         float input_movement_horizontal = Input.GetAxisRaw("Horizontal");
         float input_movement_vertical = Input.GetAxisRaw("Vertical");
 
         if (input_movement_horizontal > 0.5f || input_movement_horizontal < -0.5f)
         {
-            rb.velocity = new Vector3(input_movement_horizontal * speed, rb.velocity.y);
+            rb.velocity = new Vector3(input_movement_horizontal * new_speed, rb.velocity.y);
         }
         else if (input_movement_horizontal < 0.5f || input_movement_horizontal > -0.5f)
         {
@@ -29,14 +28,22 @@ public class Collision_Movement : MonoBehaviour {
 
         if (input_movement_vertical > 0.5f || input_movement_vertical < -0.5f)
         {
-            rb.velocity = new Vector3(rb.velocity.x, input_movement_vertical * speed);
+            rb.velocity = new Vector3(rb.velocity.x, input_movement_vertical * new_speed);
         }
         else if (input_movement_vertical < 0.5f || input_movement_vertical > -0.5f)
         {
             rb.velocity = new Vector3(rb.velocity.x, 0f);
         }
 
-
-
     }
+
+    public void Movement_Dash()
+    {
+        float input_movement_horizontal = Input.GetAxisRaw("Horizontal");
+        float input_movement_vertical = Input.GetAxisRaw("Vertical");
+
+        //rb.velocity *= dashspeed;
+
+    } 
+
 }
---
+++ TFG_CelticMithology_proj/Assets/Scripts/Player/Player_Manager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Manager : MonoBehaviour {
+
+    Collision_Movement movement_script;
+    float timer_dash = 0.0f;
+    public float timedashing = 1.0f;
+    public float speed = 5.0f;
+    public float dashspeed = 10.0f;
+
+    bool want_to_dash = false;
+
+    // Use this for initialization
+    void Start () {
+
+        movement_script = GetComponent<Collision_Movement>();
+
+        if (movement_script == null)
+        {
+            Debug.Log("Movement script not found _ Player_Manager");
+        }
+
+        timer_dash = 0.0f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            want_to_dash = true;
+        }
+
+        if (want_to_dash && timer_dash <= timedashing)
+        {
+            timer_dash += Time.deltaTime;
+            movement_script.Movement_Update(dashspeed);
+        }
+        else {
+            want_to_dash = false;
+            timer_dash = 0.0f;
+            movement_script.Movement_Update(speed);
+
+        }
+
+    }
+}
