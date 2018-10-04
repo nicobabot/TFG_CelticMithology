d1c12575d4cd35f6a56abb17b6598d9b12fcb4ad
nicobabot <nico.babotaltisen@gmail.com>
2018-10-04 14:16:16 +0200

    Player using states to make easier the player states

--- TFG_CelticMithology_proj/Assets/Scripts/Player/Collision_Movement.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/Player/Collision_Movement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Collision_Movement : MonoBehaviour {
 
     Rigidbody2D rb;
+    Player_Manager play_manager_scr;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        play_manager_scr = GetComponent<Player_Manager>();
     }
 	
 	// Update is called once per frame
-	public void Movement_Update (float new_speed) {
+	public void Movement_Update (float new_speed, bool is_dashing= false) {
 
         float input_movement_horizontal = Input.GetAxisRaw("Horizontal");
         float input_movement_vertical = Input.GetAxisRaw("Vertical");
@@ -20,6 +22,7 @@ public class Collision_Movement : MonoBehaviour {
         if (input_movement_horizontal > 0.5f || input_movement_horizontal < -0.5f)
         {
             rb.velocity = new Vector3(input_movement_horizontal * new_speed, rb.velocity.y);
+            if (!is_dashing)  play_manager_scr.current_state = Player_Manager.Player_States.MOVING_PLAYER;
         }
         else if (input_movement_horizontal < 0.5f || input_movement_horizontal > -0.5f)
         {
@@ -29,21 +32,19 @@ public class Collision_Movement : MonoBehaviour {
         if (input_movement_vertical > 0.5f || input_movement_vertical < -0.5f)
         {
             rb.velocity = new Vector3(rb.velocity.x, input_movement_vertical * new_speed);
+            if (!is_dashing) play_manager_scr.current_state = Player_Manager.Player_States.MOVING_PLAYER;
         }
         else if (input_movement_vertical < 0.5f || input_movement_vertical > -0.5f)
         {
             rb.velocity = new Vector3(rb.velocity.x, 0f);
         }
 
+        if ((input_movement_horizontal < 0.5f && input_movement_horizontal > -0.5f) && 
+            (input_movement_vertical < 0.5f && input_movement_vertical > -0.5f)) {
+            if (!is_dashing) play_manager_scr.current_state = Player_Manager.Player_States.IDLE_PLAYER;
+        }
+
     }
 
-    public void Movement_Dash()
-    {
-        float input_movement_horizontal = Input.GetAxisRaw("Horizontal");
-        float input_movement_vertical = Input.GetAxisRaw("Vertical");
-
-        //rb.velocity *= dashspeed;
-
-    } 
 
 }
--- TFG_CelticMithology_proj/Assets/Scripts/Player/Player_Manager.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/Player/Player_Manager.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Manager : MonoBehaviour {
+    public float time_dashing = 1.0f;
+    public float movement_speed = 5.0f;
+    public float dash_speed = 10.0f;
+
+    public enum Player_States
+    {
+        IDLE_PLAYER,
+        MOVING_PLAYER,
+        DASHING_PLAYER
+    }
+    public Player_States current_state;
 
     Collision_Movement movement_script;
     float timer_dash = 0.0f;
-    public float timedashing = 1.0f;
-    public float speed = 5.0f;
-    public float dashspeed = 10.0f;
-
     bool want_to_dash = false;
+
+
 
     // Use this for initialization
     void Start () {
@@ -24,6 +33,8 @@ public class Player_Manager : MonoBehaviour {
 
         timer_dash = 0.0f;
 
+        current_state = Player_States.IDLE_PLAYER;
+
     }
 	
 	// Update is called once per frame
@@ -32,18 +43,18 @@ public class Player_Manager : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             want_to_dash = true;
+            current_state = Player_States.DASHING_PLAYER;
         }
 
-        if (want_to_dash && timer_dash <= timedashing)
+        if (want_to_dash && timer_dash <= time_dashing)
         {
             timer_dash += Time.deltaTime;
-            movement_script.Movement_Update(dashspeed);
+            movement_script.Movement_Update(dash_speed, true);
         }
         else {
             want_to_dash = false;
             timer_dash = 0.0f;
-            movement_script.Movement_Update(speed);
-
+            movement_script.Movement_Update(movement_speed);
         }
 
     }
