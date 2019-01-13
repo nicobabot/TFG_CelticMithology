5693045faad439166c3828808798035725219e2f
nicobabot <nico.babotaltisen@gmail.com>
2019-01-13 11:49:50 +0100

    Pushback against wall fixed

--- TFG_CelticMithology_proj/Assets/Scripts/EnemyStuff/Actions/Action_PushBack.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/EnemyStuff/Actions/Action_PushBack.cs
@@ -6,12 +6,15 @@ public class Action_PushBack : ActionBase {
 
     public float push_distance = 1.0f;
     public float time_doing_pushback=0.5f;
+    public float min_distance = 0.1f;
 
     float timer_pushback;
     GameObject player;
     Vector3 pushback_dir = Vector3.zero;
     bool is_pushback_done = false;
     bool can_make_pushback = false;
+    bool is_there_wall = false;
+    Vector3 hitpoint_wall;
     Rigidbody2D rb;
 
     override public BT_Status StartAction()
@@ -32,6 +35,19 @@ public class Action_PushBack : ActionBase {
         Vector3 new_pos = transform.position + pushback_dir;
         Pathfinder path_scr = myBT.pathfinder_scr;
 
+        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, pushback_dir.normalized, push_distance);
+        if (hit != null)
+        {
+            foreach(RaycastHit2D hit_t in hit)
+            if (hit_t.transform.CompareTag("wall"))
+            {
+                is_there_wall = true;
+                hitpoint_wall = hit_t.point;
+                break;
+            }
+        }
+
+
         Vector3Int new_pos_cell = path_scr.walkability.LocalToCell(new_pos);
         can_make_pushback = path_scr.IsWalkableTile(new_pos_cell.x, new_pos_cell.y);
 
@@ -41,10 +57,23 @@ public class Action_PushBack : ActionBase {
         return BT_Status.RUNNING;
     }
 
+    private void OnDrawGizmos()
+    {
+        /*player = (GameObject)myBT.myBB.GetParameter("player");
+        pushback_dir = transform.position - player.transform.position;
+        pushback_dir = pushback_dir.normalized * push_distance;
+        Gizmos.DrawLine(transform.position, transform.position + pushback_dir);*/
+        Gizmos.DrawWireSphere(hitpoint_wall, 0.1f);
+    }
+
     override public BT_Status UpdateAction()
     {
-        if (!can_make_pushback)
+
+        Vector3 direction = transform.position - hitpoint_wall;
+
+        if (direction.magnitude < min_distance)
         {
+            rb.velocity = Vector2.zero;
             myBT.myBB.SetParameter("is_enemy_hit", false);
             isFinish = true;
             return BT_Status.RUNNING;
