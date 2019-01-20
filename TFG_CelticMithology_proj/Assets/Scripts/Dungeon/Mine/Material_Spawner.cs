e2e73ea3234a9bf73ae0a8629f93d1bb1bd35193
nicobabot <nico.babotaltisen@gmail.com>
2019-01-20 12:57:32 +0100

    Material spawner and type in process

---
+++ TFG_CelticMithology_proj/Assets/Scripts/Dungeon/Mine/Material_Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Material_Spawner : MonoBehaviour {
+
+    public Material_InGame material_to_spawn;
+
+    [Range(2,5)]
+    public int min_rand_materials = 2;
+    [Range(5, 15)]
+    public int max_rand_materials = 5;
+
+    CircleCollider2D col_circle;
+    bool is_spawn_done = false;
+    bool spawn_material = false;
+
+    // Use this for initialization
+    void Start () {
+        col_circle = GetComponent<CircleCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 point_to_spawn = Random.insideUnitSphere * col_circle.radius;
+
+        if (spawn_material && is_spawn_done == false)
+        {
+            switch (material_to_spawn)
+            {
+                case Material_InGame.WOOD_MATERIAL:
+                    break;
+
+                case Material_InGame.IRON_MATERIAL:
+                    break;
+
+                case Material_InGame.SILVER_MATERIAL:
+                    break;
+
+                case Material_InGame.DIAMOND_MATERIAL:
+                    break;
+            }
+            is_spawn_done = true;
+        }
+	}
+}
---
+++ TFG_CelticMithology_proj/Assets/Scripts/Dungeon/Mine/Material_Type.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Material_Type : MonoBehaviour {
+
+    public Material_InGame type;
+    public bool is_used = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (is_used)
+        {
+            //Destroy material
+            gameObject.SetActive(false);
+        }
+
+	}
+}
