e24b2cb276debbcee0604635adef54bb29d5059d
nicobabot <nico.babotaltisen@gmail.com>
2019-02-24 19:58:26 +0100

    You can create rooms and draw them

---
+++ TFG_CelticMithology_proj/Assets/Scripts/Dungeon/Procedural/ProceduralDungeonGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class ProceduralDungeonGenerator : MonoBehaviour {
+
+    static ProceduralDungeonGenerator _mapgenerator;
+
+    static public ProceduralDungeonGenerator mapGenerator
+    {
+        get
+        {
+            if(_mapgenerator == null)
+            {
+                _mapgenerator = Resources.Load("PrefabsResources/MapGenerator") as ProceduralDungeonGenerator; 
+            }
+
+            return _mapgenerator;
+        }
+    }
+
+    private static Vector2Int StarterRoomCenterPoint = Vector2Int.zero;
+
+    [Header("Map values")]
+    public Tilemap grid;
+    [SerializeField] private int tilesWidthMap;
+    [SerializeField] private int tilesHeightMap;
+    [SerializeField] private int numRooms;
+
+    [Header("Room values")]
+    public int tilesWidthRoom;
+    public int tilesHeightRoom;
+
+    public GameObject floorTile;
+
+
+    public enum TileType
+    {
+        LEFT_WALL, RIGHT_WALL, UP_WALL, DOWN_WALL,
+        FLOOR,
+        HOLE,
+    }
+
+    public enum ExitDirection
+    {
+        RIGHT_EXIT,
+        LEFT_EXIT,
+        UP_EXIT,
+        DOWN_EXIT
+    }
+
+    private void Awake()
+    {
+        _mapgenerator = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //DrawMapWithTiles();
+
+        Procedural_Room room_temp = new Procedural_Room(0,0, tilesWidthRoom, tilesHeightRoom);
+        room_temp.DrawRoom();
+
+        ExitDirectionPoint point = room_temp.exits[Random.RandomRange(0, room_temp.exits.Length)];
+
+        Procedural_Room room_temp2 = new Procedural_Room(point.nextRoomPos.x, point.nextRoomPos.y, tilesWidthRoom, tilesHeightRoom);
+        room_temp2.DrawRoom();
+
+    }
+
+    void DrawMapWithTiles()
+    {
+        Vector2Int _startingPos = new Vector2Int((int)(StarterRoomCenterPoint.x - (tilesWidthRoom * 0.5f)), (int)(StarterRoomCenterPoint.y - (tilesHeightRoom * 0.5f)));
+
+        for (int i=0; i< tilesWidthMap; i++)
+        {
+            for (int j = 0; j < tilesHeightMap; j++)
+            {
+                GameObject temp = Instantiate(floorTile, transform);
+                temp.transform.position = grid.CellToWorld(new Vector3Int(_startingPos.x + i, _startingPos.y + j, 0));
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TFG_CelticMithology_proj/Assets/Scripts/Dungeon/Procedural/Procedural_Room.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class ExitDirectionPoint
+{
+    public ProceduralDungeonGenerator.ExitDirection dir;
+    public Vector3Int nextRoomPos;
+}
+
+public class Procedural_Room : MonoBehaviour
+{
+    public ExitDirectionPoint[] exits;
+
+    private int _x_pos;
+    private int _y_pos;
+    private int _tilewidth;
+    private int _tileheight;
+
+    private ProceduralDungeonGenerator.TileType[][] room;
+
+    private int numExits = 0;
+
+    public Procedural_Room(int new_x_pos, int new_y_pos, int new_tilewidth, int new_tileheight)
+    {
+        _x_pos = new_x_pos;
+        _y_pos = new_y_pos;
+        _tilewidth = new_tilewidth;
+        _tileheight = new_tileheight;
+
+        numExits = Random.Range(1, 5);
+
+        SetExits();
+
+        room = new ProceduralDungeonGenerator.TileType[_tileheight][];
+
+        for (int i = 0; i < _tilewidth; i++)
+        {
+            room[i] = new ProceduralDungeonGenerator.TileType[_tilewidth];
+        }
+
+    }
+
+    void SetExits()
+    {
+
+        // exits = new ExitDirectionPoint[numExits];
+
+        exits = new ExitDirectionPoint[1];
+
+        for (int i = 0; i < exits.Length; i++)
+        {
+            // exits[i].dir = (ProceduralDungeonGenerator.ExitDirection)Random.Range(0, 4);
+
+            exits[i] = new ExitDirectionPoint();
+            exits[i].dir = ProceduralDungeonGenerator.ExitDirection.RIGHT_EXIT;
+
+            switch (exits[i].dir)
+            {
+                case ProceduralDungeonGenerator.ExitDirection.RIGHT_EXIT:
+                    exits[i].nextRoomPos.x = _x_pos + _tilewidth + 1;
+                    exits[i].nextRoomPos.y = _y_pos;
+                    break;
+            }
+
+        }
+
+    }
+
+    public void DrawRoom()
+    {
+        for(int i=0; i< _tilewidth; i++)
+        {
+            for(int j=0; j< _tileheight; j++)
+            {
+
+                Vector3 tile_pos = ProceduralDungeonGenerator.mapGenerator.grid.CellToWorld(new Vector3Int(_x_pos + i, _y_pos + j, 0));
+
+
+                switch (room[i][j])
+                {
+                    case ProceduralDungeonGenerator.TileType.LEFT_WALL:
+                        GameObject temp = Instantiate(ProceduralDungeonGenerator.mapGenerator.floorTile, ProceduralDungeonGenerator.mapGenerator.transform);
+                        temp.transform.position = tile_pos;
+                        break;
+                }
+            }
+        }
+    }
+
+    bool IsInsideRoom(Vector3 point)
+    {
+        bool ret = false;
+
+        if (point.x < _x_pos + _tilewidth && point.y < _y_pos + _tilewidth)
+        {
+            ret = true;
+        }
+
+        return ret;
+    }
+
+
+}
