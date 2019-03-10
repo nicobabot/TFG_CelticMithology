4f46766892ff4a6becfd7359a65fc175623889e3
HeladodePistacho <th_sola@hotmail.com>
2019-03-10 21:38:33 +0100

    Added idle animation in all directions to Distance Enemy

--- TFG_CelticMithology_proj/Assets/Scripts/EnemyStuff/BT_Enemies/BT_Caorthannach.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/EnemyStuff/BT_Enemies/BT_Caorthannach.cs
@@ -11,9 +11,23 @@ public class BT_Caorthannach : BT_Entity
     public Action_DamageStatic damage_static;
     private bool is_dead = false;
 
+    public Animator anim;
+    public GameObject player;
 
     override public void Update()
     {
+        //Debug.Log(Vector3.Dot((player.transform.position - transform.position).normalized, Vector3.up));
+
+        float value_y = Vector3.Dot((player.transform.position - transform.position).normalized, Vector3.up);
+        float value_x = Vector3.Dot((player.transform.position - transform.position).normalized, Vector3.right);
+        float direction = (value_y - (-1)) / (2);
+        anim.SetFloat("enemy_direction", direction);
+
+        Debug.Log(value_x);
+
+        if (value_x < 0.0f)
+             GetComponent<SpriteRenderer>().flipX = true;
+        else GetComponent<SpriteRenderer>().flipX = false;
 
         if ((int)myBB.GetParameter("live") <= 0)
         {
