43540b5cb41e74d7ebc1efd2ec26f3debd02313a
nicobabot <nico.babotaltisen@gmail.com>
2019-06-15 15:14:32 +0200

    Added I small tutorial

--- TFG_CelticMithology_proj/Assets/Scripts/Scene/ChangeScene.cs
+++ TFG_CelticMithology_proj/Assets/Scripts/Scene/ChangeScene.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ChangeScene : MonoBehaviour { 
+public class ChangeScene : MonoBehaviour {
+    public bool menu = false;
 
     public void Change_Scene(string scene)
     {
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        if (!menu)
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        else
+        {
+            int tutorial = PlayerPrefs.GetInt("playerTutorial");
+            if (tutorial != 1)
+            {
+                SceneManager.LoadScene("TutorialScene", LoadSceneMode.Single);
+            }
+            else
+            {
+                SceneManager.LoadScene(scene, LoadSceneMode.Single);
+            }
+        }
     }
 
     public void ExitGame()
---
+++ TFG_CelticMithology_proj/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("playerTutorial", 1);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Abutton"))
+        {
+            SceneManager.LoadScene("DungeonGenerator", LoadSceneMode.Single);
+        }
+	}
+}
